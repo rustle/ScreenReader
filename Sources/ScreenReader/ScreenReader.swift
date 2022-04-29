056e2961c0a12d7c096e1eea9e8d386ae11c4b83
Doug Russell <doug@getitdownonpaper.com>
2022-04-29 14:13:27 -0400

    Simple check for API trust

--- Sources/ScreenReader/ScreenReader.swift
+++ Sources/ScreenReader/ScreenReader.swift
@@ -12,4 +12,16 @@ public final class ScreenReader {
     static let logger = Logger(subsystem: "ScreenReader",
                                category: "ScreenReader")
     public init() {}
+    public func confirmTrust() {
+        guard isTrusted(promptIfNeeded: true) else {
+            // If you already added ScreenReader
+            // to trusted apps, it's likely that changing the
+            // binary has invalidated it's AX API access.
+            // You can usually reauthorize it by unchecking and
+            // rechecking it's entry in the list of apps
+            // with AX API access in System Preferences.
+            Self.logger.error("Not Trusted")
+            exit(1)
+        }
+    }
 }
