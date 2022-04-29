f6ea601f184554f27b5f3d44f4ba6028ea52f478
Doug Russell <doug@getitdownonpaper.com>
2022-04-29 14:15:13 -0400

    Simple wrapper for Bundle Identifiers (primarily forces lower case)

---
+++ Sources/ScreenReader/BundleIdentifier.swift
@@ -0,0 +1,49 @@
+//
+//  BundleIdentifier.swift
+//
+//  Copyright © 2017-2022 Doug Russell. All rights reserved.
+//
+
+public struct BundleIdentifier: RawRepresentable, CustomStringConvertible {
+    public typealias RawValue = String
+    public let rawValue: String
+    public let description: String
+    public init?(rawValue: String) {
+        self.rawValue = rawValue.lowercased()
+        self.description = rawValue
+    }
+    public init?(rawValue: String?) {
+        guard let rawValue = rawValue else {
+            return nil
+        }
+        self.rawValue = rawValue.lowercased()
+        self.description = rawValue
+    }
+}
+
+extension BundleIdentifier: ExpressibleByStringLiteral {
+    public typealias StringLiteralType = String
+    public init(stringLiteral value: StringLiteralType) {
+        self.init(rawValue: value)!
+    }
+}
+
+extension BundleIdentifier: Hashable {
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(rawValue)
+    }
+    public static func ==(lhs: BundleIdentifier,
+                          rhs: BundleIdentifier) -> Bool {
+        lhs.rawValue == rhs.rawValue
+    }
+    public static func ==(lhs: BundleIdentifier,
+                          rhs: String) -> Bool {
+        lhs.rawValue.compare(rhs,
+                             options: .caseInsensitive) == .orderedSame
+    }
+    public static func ==(lhs: String,
+                          rhs: BundleIdentifier) -> Bool {
+        lhs.compare(rhs.rawValue,
+                    options: .caseInsensitive) == .orderedSame
+    }
+}
