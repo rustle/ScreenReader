6272c1b7cbc95e7b907cc2e65fdfd3e9042726a2
Doug Russell <doug@getitdownonpaper.com>
2022-05-01 20:52:03 -0400

    Further refine ignored server cases

--- Sources/ScreenReader/ServerProvider.swift
+++ Sources/ScreenReader/ServerProvider.swift
@@ -1,7 +1,7 @@
 //
 //  ServerProvider.swift
 //
-//  Copyright © 2017-2021 Doug Russell. All rights reserved.
+//  Copyright © 2017-2022 Doug Russell. All rights reserved.
 //
 
 import Foundation
@@ -15,15 +15,24 @@ public actor ServerProvider {
     static let logger = Logger(subsystem: "ScreenReader",
                                category: "ServerProvider")
     // TODO: Source this from a plist or a DB
-    private let ignoreList = Set<BundleIdentifier>([
+    private let excludeList = Set<BundleIdentifier>([
         "com.apple.webkit.databases",
         "com.apple.webkit.networking",
+    ])
+    private let inclusionList = Set<BundleIdentifier>([
+        //"com.apple.finder",
     ])
     public func connect(processIdentifier: pid_t,
                         bundleIdentifier: BundleIdentifier,
                         updateFocusOnConnect: Bool = false) async throws -> Server {
         Self.logger.info("Connect \(bundleIdentifier) -- \(processIdentifier) -- updateFocusOnConnect \(updateFocusOnConnect)")
-        guard !ignoreList.contains(bundleIdentifier) && processIdentifier != getpid() else {
+        guard processIdentifier != getpid() else {
+            throw ServerProviderError.ignored
+        }
+        if !excludeList.isEmpty, excludeList.contains(bundleIdentifier) {
+            throw ServerProviderError.ignored
+        }
+        if !inclusionList.isEmpty, !inclusionList.contains(bundleIdentifier) {
             throw ServerProviderError.ignored
         }
         return try await .init(processIdentifier: processIdentifier,
